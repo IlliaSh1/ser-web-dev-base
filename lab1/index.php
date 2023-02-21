549c106ae14d38c3c407921fb479829aa432265e
IlliaSh1 <illia_sh@mail.ru>
2023-02-21 20:09:09 +0300

    init

---
+++ lab1/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Lab 1</title>
+</head>
+<body>
+  <header>
+    <img src="img/logo.svg" alt="mospolytech logo" width="300">
+  </header>
+  <main>
+
+  </main>
+  <footer>
+
+  </footer>
+  <?php
+    echo '<p><i>Hello, world!</i></p>';
+  ?>
+  
+</body>
+</html>
