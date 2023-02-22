3953d7d48a18f666417f7b1cf773ff392e383f66
IlliaSh1 <illia_sh@mail.ru>
2023-02-22 09:28:15 +0300

    fixed footer and header - lab1

--- lab1/index.php
+++ lab1/index.php
@@ -1,24 +1,39 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Lab 1</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-  <header>
-    <img src="img/logo.svg" alt="mospolytech logo" width="300">
+  <header class="header">
+    <div class="wrapper header__container">
+      <img src="img/logo.svg" alt="mospolytech logo" width="300">
+      <h1 class="title">2.1 Задание для самостоятельной работы «Hello, World!»</h1>
+      <nav class="header__menu">
+        <ul class="header__menu-list list">
+          <li class="header__menu-item"><a href="" class="link"></a></li>
+        </ul>
+      </nav>
+    </div>
   </header>
   <main>
-
+    <div class="wrapper">
+      <?php
+        echo '<h2><i>Hello, world!</i></h2>';
+      ?>
+    </div>
   </main>
-  <footer>
+  <footer class="footer">
+    <div class="wrapper footer__container">
+      <p>
+        Шамаров Илья Глебович, 221-321
+      </p>
+    </div>
 
   </footer>
-  <?php
-    echo '<p><i>Hello, world!</i></p>';
-  ?>
   
 </body>
 </html>
